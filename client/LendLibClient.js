cf750c23913515144b7547a7a602bde6f882f0c3
skrdv <skrdv@2931863.no-reply.drupal.org>
2016-03-11 01:02:54 +0300

    added accounts and owners

--- client/LendLibClient.js
+++ client/LendLibClient.js
@@ -52,7 +52,10 @@ Template.categories.events({
 		if (e.which === 13) {
 			var catVal = String(e.target.value || "");
 			if (catVal) {
-				lists.insert({Category: catVal});
+				lists.insert({
+					Category: catVal,
+					owner: Meteor.userId()
+				});
 				Session.set('adding_category', false);
 			}
 		}
@@ -159,4 +162,8 @@ Template.list.events({
 			Session.set('lendee_input',null);
 		}
 	}
+});
+
+Accounts.ui.config({
+	passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL",
 });
